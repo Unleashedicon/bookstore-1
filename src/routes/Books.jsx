c7385962ab86e0bb36dcba853903aa318e79a26d
unleashed <kkipkurui123@gmail.com>
2023-06-18 00:31:17 +0300

    add bookstore two routes

---
+++ src/routes/Books.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import BookLogic from '../components/Booklogic';
+
+const Books = () => (
+  <div className="book">
+    <Header />
+    <BookLogic />
+  </div>
+);
+
+export default Books;
---
+++ src/routes/NotMatch.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const NotMatch = () => <Navigate to="/" replace />;
+
+export default NotMatch;
