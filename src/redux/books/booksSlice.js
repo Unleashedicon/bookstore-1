91861ea7ba9a4d7c8d609594c670f9a73e9a92dc
kelvin kipkurui <kkipkurui123@gmail.com>
2023-07-29 20:11:02 +0300

    Update booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
-bookItems: [
+  bookItems: [
     {
       item_id: 'item1',
       title: 'The Great Gatsby',
